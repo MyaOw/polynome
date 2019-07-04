25c32fe804d6041e7222e4e9f1c3aa5fe0e6d8f2
Pauline Lambert <mya@mya-ow.net>
2019-07-04 23:06:57 +0200

    polygone toujours ko mais print la valeur de t

--- polynome/views.py
+++ polynome/views.py
@@ -4,16 +4,19 @@ from polynome.models import Model
 
 def index(request):
 	context = { 'title': 'ax² + bx +c',}
-	if request.method == 'GET':
-		a = 0.0
-		b = 0.0
-		c = 0.0
-		x = 0.0
-	elif request.method == 'POST':
-		a = request.POST['a']
-		b = request.POST['b']
-		c = request.POST['c']
-		x = request.POST['x']
-		t = a*x**2+(b*x)+c
 
-	return render(request, 'polynome/index.html', context)
+	if request.method == 'POST':
+		a = request.POST.get("a", False)
+		a = float(a)
+		b = request.POST.get("b", False)
+		b = float(b)
+		c = request.POST.get("c", False)
+		c = float(c)
+		x = request.POST.get("x", False)
+		x = float(x)
+		t = a*x**2+(b*x)+c
+		print(t)
+
+
+	return render(request, 'polynome/index.html', context, t)
+
